750c15313ae8c8df2f95e1bebb11fcccaf7e6050
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-08-05 09:20:32 +0530

    Get

--- src/main/java/lk/ijse/gdse/aad/spring_testmvc/api/Customer.java
+++ src/main/java/lk/ijse/gdse/aad/spring_testmvc/api/Customer.java
@@ -1,15 +1,20 @@
 package lk.ijse.gdse.aad.spring_testmvc.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/custome?")
 public class Customer {
     @PostMapping
     public String saveCustomer(){
         System.out.println("saved customer");
         return "save customer";
+    }
+    @GetMapping
+    public String getCustomer(){
+        return "get Customer with ?";
     }
 }
